4dbd16d5a32bde701d03e769029b282ce1c4f148
Jonathan <jonathanwijayan@gmail.com>
2022-09-22 07:54:40 +1000

    Got the 5 days forecast api working

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,10 @@ submitBtn.addEventListener("click", function (event) {
     console.log(weatherApi);
     getWeather(weatherApi);
 
+    var forecastApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=299a753009beb3228931e68024a5a0bc";
+    console.log(forecastApi);
+    getForecast(forecastApi);
+
     localStorage.setItem("Location", location);
 })
 
@@ -16,5 +20,25 @@ function getWeather(weatherApi) {
     fetch(weatherApi)
         .then(function (response) {
             console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            document.getElementById("city-name").textContent = data.name;
+            document.getElementById("temperature").textContent = data.main.temp;
+            document.getElementById("wind").textContent = data.wind.speed;
+            document.getElementById("humidity").textContent = data.main.humidity;
+            //document.getElementById("uv-index").textContent = data.
+        })
+}
+
+function getForecast(forecastApi) {
+    fetch(forecastApi)
+        .then(function (forecastResponse) {
+            console.log(forecastResponse);
+            return forecastResponse.json();
+        })
+        .then(function (forecastData) {
+            console.log(forecastData);
         })
 }
